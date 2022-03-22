db096a0a0a84f505e0941180269440c37fbe82df
attoyibi <muchson@alterra.id>
2022-03-22 08:57:37 +0700

    body

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <v-app class="grey lighten-4">
+  <!-- App.vue -->
+
+  <v-app>
+    <!-- -->
     <NavBar />
 
-    <v-content>
-      <router-view></router-view>
-      <BodyComponent />
-    </v-content>
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <BodyComponent />
+        <CardList />
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+      <h1>Welcome</h1>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import NavBar from "./components/NavBar.vue";
 import BodyComponent from "./components/BodyComponent.vue";
-export default {
-  name: "App",
+import NavBar from "./components/NavBar.vue";
+import CardList from "./components/CardList.vue";
 
+export default {
   data: () => ({
-    //
+    reveal: false,
+    show: false,
   }),
   components: {
-    NavBar,
     BodyComponent,
+    NavBar,
+    CardList,
   },
 };
 </script>
+
+<style>
+.v-card--reveal {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
+}
+</style>
--- src/components/BodyComponent.vue
+++ src/components/BodyComponent.vue
@@ -2,14 +2,16 @@
   <v-container>
     <v-row align="center" justify="right">
       <v-col>
-        <v-card
-          img="https://cdn.vuetifyjs.com/images/cards/girl.jpg"
-          height="300"
-          class="portrait"
-        >
-          <v-btn color="yellow">
-            <v-icon color="white">Subscribe</v-icon>
-          </v-btn>
+        <v-card height="371" style="color: white" class="mx-auto background">
+          <v-container fill-height fluid>
+            <v-col style="margin-left: 50%" cols="12" align="left">
+              <h1 style="font-size: 50px">The Art Of Travel</h1>
+              <h4>Finding Beauty, one flight at a time</h4>
+              <v-btn style="margin-top: 20px" small color="warning" dark>
+                Normal Button
+              </v-btn>
+            </v-col>
+          </v-container>
         </v-card>
       </v-col>
     </v-row>
@@ -19,6 +21,28 @@
 /* ✓ GOOD */
 export default {
   name: "BodyComponent",
+  data: () => ({
+    //
+  }),
   // ...
 };
 </script>
+<style>
+.card {
+  background: url("https://ohlaladani.com.br/wp-content/uploads/wallpaper-OHLALADANI_DESKTOP_WALLPAPERS_AVENTURA-2.jpg")
+    no-repeat center center fixed !important;
+  background-size: cover !important;
+}
+.background {
+  background: url("https://ohlaladani.com.br/wp-content/uploads/wallpaper-OHLALADANI_DESKTOP_WALLPAPERS_AVENTURA-2.jpg")
+    no-repeat center center fixed !important;
+  background-size: cover !important;
+}
+.white {
+  color: white;
+}
+.btn-color {
+  background-color: red !important;
+  color: red !important;
+}
+</style>
