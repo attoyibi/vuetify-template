1f9ccf63f522f3cce8be148fb4e2bd6ff5df5438
attoyibi <muchson@alterra.id>
2022-03-27 14:18:31 +0700

    firebase get all data

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input type="text" id="nameField" ref="nameField" />
+    <input type="text" id="ageField" ref="ageField" />
+    <button v-on:click="writeData()">Submit</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
+// // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHF8IaAmvmr4uj4iue-jt3ORXKSfM2rNA",
+  authDomain: "custom-bdcb0.firebaseapp.com",
+  databaseURL: "https://custom-bdcb0-default-rtdb.firebaseio.com",
+  projectId: "custom-bdcb0",
+  storageBucket: "custom-bdcb0.appspot.com",
+  messagingSenderId: "1014173285269",
+  appId: "1:1014173285269:web:1d9dafd7f86cf6e5e3d49c",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+//init services
+const db = getFirestore();
+
+//collection ref
+const colRef = collection(db, "books");
+
+//get collection data
+getDocs(colRef).then((snapshot) => {
+  let books = [];
+  snapshot.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id });
+  });
+  console.log(books);
+});
+console.log(getDocs);
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  methods: {
+    writeData(e) {
+      // e.preventDefault();
+      console.log(e);
+      // addDoc(colRef, {
+      //   title: e.title.value,
+      //   author: "addBookForm.author.value",
+      // }).then(() => {});
+    },
+  },
+};
 </script>
 
 <style>
