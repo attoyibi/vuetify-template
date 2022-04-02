3012c02c05c620be9a700ab0f541325ed29dac64
attoyibi <muchson@alterra.id>
2022-04-03 00:03:08 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,12 @@
       <!-- specify the link by passing the `to` prop. -->
       <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
       <router-link to="/">Go to Home</router-link>
-      <router-link to="/about">Go to About</router-link>
+      <router-link to="/home">Go to About</router-link>
     </p>
     <!-- route outlet -->
     <!-- component matched by the route will render here -->
 
-    <router-view> </router-view>
+    <router-view></router-view>
   </div>
 </template>
 
