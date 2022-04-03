ee061913ba8599dccbe3b7a5c5b241b79c5d3a7a
attoyibi <muchson@alterra.id>
2022-04-03 20:27:10 +0700

    update store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
-import Vue from "vue";
-import { Vuex } from "vuex";
-Vue.use(Vuex);
-const store = Vuex.Store({
-  state: {
-    appName: "Vue 2 vuex",
-  },
+// // import Vue from "vue";
+// import { Vuex } from "vuex";
+// // Vue.use(Vuex);
+// const store = new Vuex.Store({
+//   state: {
+//     appName: "Vue 2 vuex",
+//   },
 
+//   getters: {
+//     appName: (state) => state.appName,
+//   },
+// });
+
+// export default store;
+
+import Vue from "vue";
+import Vuex from "vuex";
+Vue.use(Vuex);
+const state = {
+  appName: "Muchson vuex",
+};
+export default new Vuex.Store({
+  state,
   getters: {
     appName: (state) => state.appName,
   },
 });
-
-export default store;
